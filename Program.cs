10a02b7e964b9338f2d8b76daa227ec580f42688
kdoranco <rk_doranco-cda@gmx.com>
2020-04-17 13:22:33 +0200

    1er Squellette du code souce de l'Appli

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjetGitQCM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Début de Notre application de QCM sur GIT\n");
+        }
+    }
+}
